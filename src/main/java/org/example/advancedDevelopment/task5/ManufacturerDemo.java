644c8b756610d901a87e6049121f8ba50681402f
Patka879 <mysiak.patrycja@gmail.com>
2023-05-09 13:04:14 +0200

    Manufacturer class created

---
+++ src/main/java/org/example/advancedDevelopment/task5/ManufacturerDemo.java
@@ -0,0 +1,8 @@
+package org.example.advancedDevelopment.task5;
+
+public class ManufacturerDemo {
+    public static void main(String[] args) {
+        Manufacturer manufacturer1 = new Manufacturer("Biomet Inc.", 1992, "U.S");
+        System.out.println(manufacturer1.hashCode());
+    }
+}
