f9bfe45ebbd1546ea75c354a15cd4742f47c0ba5
Patka879 <mysiak.patrycja@gmail.com>
2023-05-10 14:18:29 +0200

    Test cases added

--- src/test/java/Task9Tests.java
+++ src/test/java/Task9Tests.java
@@ -1,7 +1,11 @@
-import org.example.advancedDevelopment.task9.Joiner;
 import org.example.advancedDevelopment.task9.JoinerInteger;
 import org.example.advancedDevelopment.task9.JoinerString;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,5 +22,34 @@ public class Task9Tests {
         JoinerString joinerString = new JoinerString("-");
         String result = joinerString.join("1","2","3");
         assertThat(result).isEqualTo("[0:text1-1:text2-2:text3]");
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void checkMultipleCasesOfIntegerJoiner(String separator, String expectedResult) {
+        JoinerInteger joinerInteger = new JoinerInteger(separator);
+        String result = joinerInteger.join(0,1,2,3,4);
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    public static Stream<Arguments> checkMultipleCasesOfIntegerJoiner() {
+        return Stream.of(
+                Arguments.of(",", "{0,1,2,3,4}"),
+                Arguments.of("-", "{0-1-2-3-4}")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource
+    public void checkMultipleCasesOfStringJoiner(String expectedResult, String[] things) {
+        JoinerString stringJoiner = new JoinerString("-");
+        String result = stringJoiner.join(things);
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    public static Stream<Arguments> checkMultipleCasesOfStringJoiner() {
+        return Stream.of(
+                Arguments.of("[0:text1-1:text2-2:text3]", new String[]{"1", "2", "3"}),
+                Arguments.of("[0:text5-1:text7-2:text9]", new String[]{"5", "7", "9"})
+        );
     }
 }
